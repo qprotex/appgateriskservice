35644d93b88287621e3af72502f27c9f427a2a68
Q- Protex´s Repository <mfebres@q-protex.com>
2021-12-11 14:04:18 -0500

    Initial commit

---
+++ db.py
@@ -0,0 +1,23 @@
+import sqlalchemy.pool
+from sqlalchemy import create_engine
+
+from flask import current_app
+
+engine = create_engine('sqlite://',
+                    connect_args = {'check_same_thread': False},
+                    poolclass = sqlalchemy.pool.StaticPool)
+
+def get_db():
+    return engine
+
+
+def init_db():
+    with engine.connect() as c:
+        with current_app.open_resource('schema.sql') as f:
+            lines = f.readlines()
+            for line in lines:
+                c.execute(line.decode('utf8'))
+
+
+def init_app(app):
+    init_db()
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Users (id, username);
+INSERT INTO Users VALUES (1, 'UserA');
+
+CREATE TABLE UserComputers (id, userId, computerFingerPrint);
+INSERT INTO UserComputers VALUES (1, 1, '123');
