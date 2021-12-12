48e6325611cf5675a39ddb9700424306a01f002f
Q- Protex´s Repository <mfebres@q-protex.com>
2021-12-12 15:29:27 -0500

    SQLAlchemy for ORM isclientknown implementation

--- db.py
+++ db.py
@@ -1,23 +1,88 @@
 import sqlalchemy.pool
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+# from flask import current_app
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 
-from flask import current_app
-
+Base = declarative_base()
 engine = create_engine('sqlite://',
                     connect_args = {'check_same_thread': False},
                     poolclass = sqlalchemy.pool.StaticPool)
 
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
 
-def get_db():
-    return engine
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+
+
+
+class LogData(Base):
+    __tablename__ = 'logdata'
+    id = Column(Integer, primary_key=True)
+    source = Column(String)
+    username = Column(String)
+    ip = Column(String)
+    fingerPrint = Column(String)
+    createdDate = Column(String)
+
+
+class CompanyNetwork(Base):
+    __tablename__ = 'companynetwork'
+    id = Column(Integer, primary_key=True)
+    range = Column(String)
+
+
+class UserIP(Base):
+    __tablename__ = 'userip'
+    id = Column(Integer, primary_key=True)
+    ip = Column(String)
+
+
+class UserClient(Base):
+    __tablename__ = 'userclient'
+    id = Column(Integer, primary_key=True)
+    fingerPrint = Column(String)
+
+
+def get_db_session():
+    return session
+    #return engine
 
 
 def init_db():
-    with engine.connect() as c:
-        with current_app.open_resource('schema.sql') as f:
-            lines = f.readlines()
-            for line in lines:
-                c.execute(line.decode('utf8'))
+    Base.metadata.create_all(engine)
+
+    user = User(username='UserA', password=generate_password_hash('1234', method='sha256'))
+    session.add(user)
+
+    userip = UserIP(ip='10.97.2.10')
+    session.add(userip)
+
+    ud = UserClient(fingerPrint='DefaultFingerPrint')
+    session.add(ud)
+
+    cn = CompanyNetwork(range='10.97.2.0/24')
+    session.add(cn)
+
+    cn = LogData(source='Computer', username='UserA', ip='10.97.2.10', fingerPrint='DefaultFingerPrint',
+                 createdDate=datetime.now().timestamp())
+    session.add(cn)
+
+    session.commit()
+    # with engine.connect() as c:
+    #     with current_app.open_resource('schema.sql') as f:
+    #         lines = f.readlines()
+    #         for line in lines:
+    #             c.execute(line.decode('utf8'))
 
 
 def init_app(app):
--- schema.sql
+++ schema.sql
@@ -1,12 +1,18 @@
-CREATE TABLE Users (id int, username text);
+CREATE TABLE Users (id INTEGER PRIMARY KEY, username text);
 INSERT INTO Users VALUES (1, 'UserA');
 
-CREATE TABLE UserComputers (id int, userId int, fingerPrint text);
+CREATE TABLE UserComputers (id INTEGER PRIMARY KEY, userId INTEGER, fingerPrint text);
 INSERT INTO UserComputers VALUES (1, 1, 'PostmanRuntime/7.28.4');
 INSERT INTO UserComputers VALUES (2, 1, 'Custom User Agent');
 
-CREATE TABLE CompanyNetwork (id int, range text);
+
+CREATE TABLE UserStats (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, lastsuccessfullogindate INTEGER);
+INSERT INTO UserStats VALUES (1, 1, strftime('%s','2021-12-1'));
+
+CREATE TABLE CompanyNetwork (id INTEGER PRIMARY KEY, range text);
 INSERT INTO CompanyNetwork VALUES (1, '10.97.2.0/24');
 
-CREATE TABLE LogData (id int, username text, ip text, fingerPrint text, createdDate int, loginStatus int);
-INSERT INTO LogData VALUES (1, 'UserA', '10.97.2.10', 'PostmanRuntime/7.28.4', strftime('%s','now'), true);
+CREATE TABLE LogData (id INTEGER PRIMARY KEY AUTOINCREMENT, source text, username text, ip text, fingerPrint text, createdDate INTEGER, loginStatus INTEGER);
+INSERT INTO LogData VALUES (1, 'website', 'UserA', '10.97.2.10', 'PostmanRuntime/7.28.4', strftime('%s','2021-12-10'), true);
+INSERT INTO LogData VALUES (2, 'mobile', 'UserA', '10.97.2.10', 'PostmanRuntime/7.28.4', strftime('%s','now'), true);
+INSERT INTO LogData VALUES (3, 'mobile', 'UserA', '10.97.2.10', 'PostmanRuntime/7.28.4', strftime('%s','now'), true);
